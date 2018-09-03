fabd0efbf5e52b4bc6963546dc88235fa37ec133
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-02 19:21:00 -0500

    Update timer

--- src/timer.c
+++ src/timer.c
@@ -1,12 +1,10 @@
 #include "timer.h"
-static struct timespec set;
+static clock_t then;
 void start_timer()
 {
-	clock_gettime(CLOCK_MONOTONIC,&set);
+	then=clock();
 }
 double read_timer()
 {
-	struct timespec end;
-	clock_gettime(CLOCK_MONOTONIC,&end);
-	return (end.tv_sec-set.tv_sec)+(end.tv_nsec-set.tv_nsec)/1e9;
+	return (clock()-then)/(double)CLOCKS_PER_SEC;
 }
